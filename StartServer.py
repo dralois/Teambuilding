a65675b59d96a8334cf955ca5d75a54ef8b31828
VFelix <ge38xes@mytum.de>
2020-06-08 17:59:20 +0200

    Added simple server

--- StartServer.py
+++ StartServer.py
@@ -1,5 +1,21 @@
+import http.server
+import socketserver
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+
 def start():
-    print("Hello World")
+    PORT = 8080
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
 
 
 if __name__ == "__main__":
