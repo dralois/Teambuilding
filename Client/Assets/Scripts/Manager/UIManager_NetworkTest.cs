be46a9ef8ca934ecf42b149d1b3b6bbe4f43cb8c
dralois <epple.alexander@web.de>
2020-07-18 18:44:12 +0200

    Added basic http client + test + scene

---
+++ Client/Assets/Scripts/Manager/UIManager_NetworkTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Unity.UIElements.Runtime;
+
+public class UIManager_NetworkTest : MonoBehaviour
+{
+	public PanelRenderer testUI;
+
+	private void OnEnable()
+	{
+		testUI.postUxmlReload = X_BindTestUI;
+	}
+
+	private IEnumerable<UnityEngine.Object> X_BindTestUI()
+	{
+		var root = testUI.visualTree;
+		var sendButton = root.Q<Button>("send-request");
+		sendButton.clickable.clicked += () =>
+		{
+			StartCoroutine(HttpClient.CallMethod("TestMethod", new Dictionary<string, string>() { { "TestKey", "TestVal" } }));
+		};
+		return null;
+	}
+}
---
+++ Client/Assets/Scripts/Network/HttpClient.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HttpClient : MonoBehaviour
+{
+	public static string serverAdress = "http://212.83.56.221:8080/";
+
+	public static IEnumerator CallMethod(string method, Dictionary<string, string> data)
+	{
+		string uri = serverAdress + UnityWebRequest.EscapeURL(method);
+		using (var request = UnityWebRequest.Post(uri, data))
+		{
+			Debug.Log($"Sending {uri}");
+			yield return request.SendWebRequest();
+			if (request.isDone)
+			{
+				if(request.responseCode == 200)
+				{
+					var headers = request.GetResponseHeaders();
+					Debug.Log($"Recieved {request.responseCode}");
+					foreach(var header in headers)
+					{
+						Debug.Log($"{header.Key} : {header.Value}");
+					}
+				}
+				else
+				{
+					Debug.LogError($"Response error: {request.responseCode}");
+				}
+			}
+			else
+			{
+				Debug.LogError($"Request error: {request.error}");
+			}
+		}
+	}
+}
