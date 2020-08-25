c79794994bc7927367b908ba370363ee4bff3717
Alexander <epple.alexander@web.de>
2020-08-25 05:17:30 +0200

    fix

--- Server/ServerModule/StartServer.py
+++ Server/ServerModule/StartServer.py
@@ -27,7 +27,9 @@ class GameHandler(BaseHTTPRequestHandler):
         self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Access-Control-Allow-Credentials", "true")
         self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-        self.send_header("Access-Control-Allow-Headers", "Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time")
+        self.send_header("Access-Control-Request-Methods", "GET, POST, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time, room_id, picture, success, pers_id, name, ready, no_user, no_ready, gamestate, participants, places, selected, range, reason")
+        self.send_header("Access-Control-Request-Headers", "Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time, room_id, picture, success, pers_id, name, ready, no_user, no_ready, gamestate, participants, places, selected, range, reason")
 
     def do_GET(self):
         print("GET")
